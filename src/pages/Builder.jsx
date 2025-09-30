174169107bb504c6700bd01ce90179f78d359dcf
Sbusiso2904 <sbusisombatha2904@gmail.com>
2025-09-30 22:46:11 +0200

    build in the building

--- src/pages/Builder.jsx
+++ src/pages/Builder.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Sidebar({ isOpen, toggleSidebar }) {
+  
   return (
     <>
       <div className={`fixed top-0 left-0 h-full w-64 bg-green-800 text-white transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-50`}>
