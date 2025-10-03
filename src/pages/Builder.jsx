f815b85db50e5fd61177f5bd5b5d18c42a4dcadb
Sbusiso2904 <sbusisombatha2904@gmail.com>
2025-10-03 13:15:22 +0200

    added 404 page

--- src/pages/Builder.jsx
+++ src/pages/Builder.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 function Sidebar({ isOpen, toggleSidebar }) {
   return (
+    
     <>
       <div className={`fixed top-0 left-0 h-full w-64 bg-green-800 text-white transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-50`}>
         <div className="p-6">
