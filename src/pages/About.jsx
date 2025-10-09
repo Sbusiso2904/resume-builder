fdbf0f7d96d98b21fa0fc0b37d5a6b44b80f85c3
Sbusiso2904 <sbusisombatha2904@gmail.com>
2025-10-09 22:32:19 +0200

    About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function About() {
   return (
     <div className="bg-white p-8 rounded-lg shadow-md">
-      <h2 className="text-3xl font-bold text-green-900 mb-4">Welcome to Your Dashboard!</h2>
-      <p className="text-gray-600 text-lg">You have successfully logged in to your Resume Builder dashboard.</p>
+      <h2 className="text-3xl font-bold centre-text-green-900 mb-4">About Us</h2>
+      <p className="text-gray-600 text-lg">All you need to know about Resume SA.</p>
       
     </div>
   );
