21afe8b7f1933d733ef2cb1abb571a78ff743497
Sbusiso2904 <sbusisombatha2904@gmail.com>
2025-10-09 22:37:47 +0200

    dealing with about pagee

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function About() {
   return (
-    <div className="bg-white p-8 rounded-lg shadow-md">
-      <h2 className="text-3xl font-bold centre-text-green-900 mb-4">About Us</h2>
+    <div className="bg-white p-20 rounded-lg shadow-md">
+      <h2 className="text-3xl centre-font-bold text-green-900 mb-4">About Us</h2>
       <p className="text-gray-600 text-lg">All you need to know about Resume SA.</p>
       
     </div>
