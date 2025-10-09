36c40a564853aad638d6c8278beeafa80f684769
Sbusiso2904 <sbusisombatha2904@gmail.com>
2025-10-09 22:02:52 +0200

    dealing with about pagee

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,7 @@ function About() {
   return (
     <div className="max-w-2xl mx-auto p-6 ">
       <h1 className="text-3xl font-bold text-gray-800 mb-4 flex justify-center">About</h1>
-      <p className="text-gray-700 text-lg leading-relaxed flex justify-center">
-        Hello there.<br />
-        How do you do?
 
-      </p>
     </div>
   );
 }
